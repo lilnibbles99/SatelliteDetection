d961d537948ee8c4d4c4df6a8a3cb0f3324ff480
lilnibbles99 <147742777+lilnibbles99@users.noreply.github.com>
2023-10-22 20:50:01 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 ####Importing everything we need
 
-import fitparse
+#import fitparse
+from astropy.io import fits
 import cv2
 import numpy as np
 from skimage.feature import blob_log
 
 ####Edit these ones
 
-threshold1 = 150 #150
+threshold1 = 90 #150
 threshold2 = 100 #100
-minlinelength = 150 #150
-maxlinegap = 5  #5
+edge1 = 50 #50
+edge2 =  150 #150
+minlinelength = 5 #150
+maxlinegap = 50  #5
 threshold3 = 0.1 #0.1
 
 ####Creating arrays early and opening the image file
@@ -19,31 +22,22 @@ coords = []
 lines_list = []
 x_coords = np.array([])
 y_coords = np.array([])
-FIT_image = fitparse.FitFile("C:/Users/thegr/Desktop/WORK/year 3/python data/SATELLITES/2023-01-13-1830_7-CapObj_0026.FIT")
-for name in fit_file.get_messages("record"):
-    for data in name:
-        if data.name == "timestamp":
-            time.append(data.value)
-        elif data.name == "":
-        elif data.name == "":
-print(FIT_image)
-image = np.array(data)
-
-#image = cv2.imread('C:/Users/thegr/Desktop/WORK/year 3/python data/SATELLITE/2023-01-13-1835_4-CapObj_0048.FIT')
-
-print(type(image))
+with fits.open("C:/Users/thegr/Desktop/WORK/year3/python data/SATELLITES/2023-01-13-1830_7-CapObj_0026.FIT") as hdu:
+    image = hdu[0].data
+image = cv2.resize(image, dsize=(1920,1080), interpolation=cv2.INTER_CUBIC)
+image = (image/256).astype(np.uint8)
 cv2.imshow("image", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-####Processing the image to make it grayscale and only caring about the boundaries of the shapes
+####Processing the image to only caring about the boundaries of the shapes
 
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-threshold, im_bw = cv2.threshold(gray, threshold1, 255, cv2.THRESH_TOZERO)
-edges = cv2.Canny(im_bw,50,150, apertureSize = 3)
-new_image = im_bw
-cv2.imshow("gray",gray)
+threshold, im_bw = cv2.threshold(image, threshold1, 255, cv2.THRESH_TOZERO)
+cv2.imshow("im_bw",im_bw)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
+edges = cv2.Canny(im_bw,edge1,edge2, apertureSize = 3)
+new_image = im_bw
 cv2.destroyAllWindows()
 cv2.imshow("edges",edges)
 cv2.waitKey(0)
@@ -75,16 +69,6 @@ for blob in blobs:
 cv2.imshow("stars",color_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-########### This is old code I am keeping while I bugfix the code above
-#for rows in range(new_image.shape[0]):
-#    for columns in range(new_image.shape[1]):
-#        if new_image[rows][columns] != 0:
-#            x_coords = np.concatenate((x_coords, np.array([columns])))
-#            y_coords = np.concatenate((y_coords, np.array([rows])))
-#x_coords = x_coords.reshape(-1, 1)
-#y_coords = y_coords.reshape(-1, 1)
-#coords = np.hstack((y_coords, x_coords))
 
 ####Comparing the star map to the image in question and finding the transformation
 
