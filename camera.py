a244f26e9184599d59b689f5bb4d4564673400f5
lilnibbles99 <147742777+lilnibbles99@users.noreply.github.com>
2023-10-23 14:20:31 +0100

    camera.py

---
+++ camera.py
@@ -0,0 +1,13 @@
+import cv2
+
+exposure_time = 10000 #in ms
+
+camera = cv2.VideoCapture(0)
+print(camera.isOpened())
+camera.set(cv2.CAP_PROP_EXPOSURE, exposure_time)
+print(camera.get(cv2.CAP_PROP_EXPOSURE))
+stat, image = camera.read()
+if stat == True:
+    cv2.imshow("image",image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
