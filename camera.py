2f9205025957c4e3213a88748745d400b73ee37b
LHMartin2023 <theo.fov@gmail.com>
2023-10-23 15:12:39 +0100

    Update camera.py

--- camera.py
+++ camera.py
@@ -2,7 +2,7 @@ import cv2
 
 exposure_time = 10000 #in ms
 
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(1)
 print(camera.isOpened())
 camera.set(cv2.CAP_PROP_EXPOSURE, exposure_time)
 print(camera.get(cv2.CAP_PROP_EXPOSURE))
